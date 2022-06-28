5756acfc29293edf019ceadd2ddf9e2c445599f0
Harikrishna2699 <73926065+Harikrishna2699@users.noreply.github.com>
2022-06-28 16:30:59 +0530

    Add files via upload

---
+++ ascii val of char.py
@@ -0,0 +1,10 @@
+# Python program to print
+# ASCII Value of Character
+
+# In c we can assign different
+# characters of which we want ASCII value
+
+a='h'
+# print the ASCII value of assigned character in c
+print("The ASCII value   is", ord(a))
+
