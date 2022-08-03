f29e855daa3bded58f09bc4fbb2b419fa59738a2
Harikrishna Raghu <73926065+Harikrishna2699@users.noreply.github.com>
2022-08-03 11:23:22 +0530

    Add files via upload

---
+++ while loop.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#while loop
+
+
+# In[3]:
+
+
+#print n natural numbers
+n=int(input("enter the n value"))
+ 
+i=0  #------->loop variable initialization
+
+while(i<=n): #---------> condition
+    print(i)
+    i=i+1 #----> updation
+    
+print("end of loop")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
