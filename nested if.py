61510b64e54e88473067ea05131a4a7c7211b444
Harikrishna Raghu <73926065+Harikrishna2699@users.noreply.github.com>
2022-08-03 11:13:16 +0530

    Add files via upload

---
+++ nested if.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#nested if
+
+
+# In[5]:
+
+
+a=10
+b=58
+c=45
+if(a>b):
+    if(a>c):
+        print('a is large')
+    else:
+        print('c is large')
+    
+elif(b>c):
+     print('b is large')
+    
+else:
+      print('c is large')
+    
+
+
+# In[ ]:
+
+
+
+
